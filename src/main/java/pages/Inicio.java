e358a4eb0ee9b766e3300dd6601e46c91d8a5590
vanebrina <seider.sanin22135@ucaldas.edu.co>
2024-04-12 15:35:56 -0500

    create inicio page model

---
+++ src/main/java/pages/Inicio.java
@@ -0,0 +1,11 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class Inicio {
+    private WebDriver driver;
+
+    public Inicio(WebDriver driver) {this.driver = driver;}
+
+
+}
--- src/test/java/edestinosTests.java
+++ src/test/java/edestinosTests.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 
@@ -12,14 +13,19 @@ public class edestinosTests {
     @BeforeAll
     public static void configuracionGeneral() {
 //        System.setProperty("webdriver.edge.driver", "C:\\edgedriver_win64\\msedgedriver.exe");
-//        System.setProperty("webdriver.edge.driver", "C:\\Taller Software\\edgedriver_win64\\msedgedriver.exe");
+        System.setProperty("webdriver.edge.driver", "C:\\Taller Software\\edgedriver_win64\\msedgedriver.exe");
     }
 
     @BeforeEach
     public void abrirNavegador() {
         driver = new EdgeDriver();
-        driver.get("");
+        driver.get("https://www.edestinos.com.co/");
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+    }
+
+    @Test
+    void prueba1() {
+
     }
 
 }
